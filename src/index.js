a5add92bffb325203a806c3cc6c5ef48e8b803e6
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-11-18 14:27:09 +1100

    middleware basic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import uuidv4 from 'uuid/v4';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -29,7 +30,15 @@ let messages = {
   },
 };
 
+// Middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
+// End Middleware
 
 app.get('/users', (req, res) => {
   return res.send(Object.values(users));
@@ -55,9 +64,39 @@ app.delete('/users/:userId', (req, res) => {
 app.get('/messages', (req, res) => {
   return res.send(Object.values(messages));
 });
+
 app.get('/messages/:messageId', (req, res) => {
   return res.send(messages[req.params.messageId]);
 });
+
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = messages;
+
+  messages = otherMessages;
+
+  return res.send(messages);
+});
+
+app.get('/session', (req, res) => {
+  return res.send(users[req.me.id]);
+});
+
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
