9b847f7860a17c92600338ced52f0c5c803b1e44
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-11-23 18:08:53 +1100

    finished basics

--- src/index.js
+++ src/index.js
@@ -14,10 +14,10 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   req.context = {
     models,
-    me: models.users[1]
+    me: await models.User.findbyLogin('tnguyen1')
   };
   next();
 });
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,12 +2,25 @@ import Router from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.send(Object.values(req.context.models.users));
+router.get('/', async (req, res) => {
+  const users = await req.context.models.User.find();
+  return res.send(users);
 });
 
-router.get('/:userId', (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
+router.get('/:userId', async (req, res) => {
+  const user = await req.context.models.User.findbyId(req.params.userId);
+  return res.send(user);
+});
+
+router.delete('/:userId', async (req, res) => {
+  const user  = await req.context.models.User.findById(req.params.userId);
+
+  let result = null;
+  if(user) {
+    result = await user.remove();
+  }
+
+  return res.send(result);
 });
 
 export default router;
